943ffea05e04c91569d6489b4d09c8a5507cc099
jmskrrttt <jimmgofedy@gmail.com>
2022-04-21 15:47:12 +0700

    tugas

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+ <?php
+ class contoh1 extends CI_Controller
+ {
+     public function index()
+     {
+        echo "<h1> PERKENALKAN </h1>";
+        echo " Nama Saya Hendro Aji <br> Saya Tinggal di Ciledug <br> Hobi saya main air ";
+     }
+     
+ }
+ 
---
+++ application/views/v_about.php
@@ -0,0 +1,25 @@
+<section>
+    <h1>
+       <?php echo $judul ?></h1>
+       <h4>Nama</h4>
+       <ul type="disc">
+           <li>Nama Depan : Hendro </li>
+           <li>Nama Belakang : Aji</li>
+        </ul>
+        <br>
+        <h4>Alamat</h4>
+        <ul type="none">
+            <li> Jalan Haji Kane</li>
+        </ul>
+
+        <h4>Tempat Lahir</h4>
+        <ul type="none">
+            <li>Kasur RS</li>
+        </ul>
+
+        <h4>Olah Raga Favorit</h4>
+        <ul type="square">
+            <li>Bermain Catur</li>
+        
+        </ul>
+</section>
